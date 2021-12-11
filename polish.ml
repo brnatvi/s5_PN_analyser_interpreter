533a7587ce0e07c015382f39c3bcc97d93806a43
BRAGINA natalia <nata.br@gmail.com>
2021-12-11 19:23:21 +0100

    block constructor & reprint

--- polish.ml
+++ polish.ml
@@ -5,7 +5,7 @@
     polish.ml. Il est recommandé d'architecturer ultérieurement votre
     projet en plusieurs fichiers source de tailles raisonnables *)
 
-(*****************************************************************************)
+(**********************************************************************************************************************)
 (** Syntaxe abstraite Polish (types imposés, ne pas changer sauf extensions) *)
 
 (** Position : numéro de ligne dans le fichier, débutant à 1 *)
@@ -14,6 +14,8 @@ type position = int
 (** Nom de variable *)
 type name = string
 
+(** Type for sliced and splited code *)
+type code_lines_t = (int * (string list)) list
 
 (** Opérateurs arithmétiques : + - * / % *)
 type op = Add | Sub | Mul | Div | Mod
@@ -51,16 +53,16 @@ type program = block
 type sign = Neg | Zero | Pos | Error
 
 module NameTable = Map.Make(String)
+let empty_string : int = -1
 
-module NameSet   = Set.Make(String)
-
-(************************  Exceptions  ******************************)
+(***********************************************  Exceptions  *********************************************************)
 
 exception ErrorCountOffsets of int
 exception FoundOpName of op
 exception WrongResult of string
+exception WrongSyntax of string
 
-(**********************  Syntax check up  *******************************)
+(******************************************  Syntax check up  *********************************************************)
 let get_op (st : string) : op =
   match st with
   | "+" -> Add
@@ -70,7 +72,7 @@ let get_op (st : string) : op =
   | "%" -> Mod
   | _   -> raise (WrongResult ("'"^st^"'"^" is not an operation"))
 
-let get_comp (st : string) : comp =
+  let get_comp (st : string) : comp =
   match st with
   | "="  -> Eq
   | "<>" -> Ne
@@ -78,42 +80,49 @@ let get_comp (st : string) : comp =
   | "<=" -> Le
   | ">"  -> Gt
   | ">=" -> Ge
-  |  _   -> raise (WrongResult ("'"^st^"'"^" is not a comparaison"))
-  
-let analize_variable (st : string) : name =
-    if ((st.[0] = '+') || (st.[0] = '-') || (st.[0] = '*') || (st.[0] = '/') || (st.[0] = '%')) then failwith "not a variable"
-    else st 
+  |  _   -> raise (WrongResult ("'"^st^"'"^" is not a comparaison")) 
 
-let check_assignment (st : string) : bool =  
-  (st.[0] = ':') && (st.[1] = '=')
+let get_name (e : expr) : name =
+  match e with 
+  | Var n -> n
+  | _ -> raise (WrongResult (" not a variable"))
 
-let get_name_or_var a =
+let get_name_or_var ( a : string) : expr =
     match (int_of_string_opt a) with
     | Some x -> Num x
     | None -> 
       match a with
-      | "+" -> raise (FoundOpName (Add))
-      | "-" -> raise (FoundOpName (Sub))
-      | "*" -> raise (FoundOpName (Mul))
-      | "/" -> raise (FoundOpName (Div))
-      | "%" -> raise (FoundOpName (Mod))
+      | "+" -> raise (FoundOpName Add)
+      | "-" -> raise (FoundOpName Sub)
+      | "*" -> raise (FoundOpName Mul)
+      | "/" -> raise (FoundOpName Div)
+      | "%" -> raise (FoundOpName Mod)
       | var -> Var var
 
-let truncateHead (lst : 'a list) (count : int) : 'a list =
-  List.filteri (fun x _ -> x >= count) lst
+(* remove first c elements from list and return obtained list *)
+let truncateHead (l : 'a list) (c : int) : 'a list =
+  List.filteri (fun x _ -> x >= c) l
 
-let analize_expr (l : string list) : expr =   
+(* remove element el from list and return obtained list *)
+let removeEl (l : string list) (el : string) : 'a list =
+    List.filter (fun x -> x <> el) l
+
+(* analyse syntaxe to compose expression *)
+let analize_expr (l : string list) : expr =                 (*TODO  List.length stack calculate before*)
   let rec aux list stack =  
     match list with
-    | [] -> List.nth stack 0
+    | [] -> if List.length stack = 1 then List.nth stack 0
+            else let err_msg = Printf.sprintf "Expression has more than expected tokens: %d" (List.length stack) in 
+            raise (WrongResult err_msg)
     | h::tail ->
       try let s = (get_name_or_var h) in aux tail (s::stack)
-    with FoundOpName opName ->
-        aux tail (Op(opName, (List.nth stack 0), (List.nth stack 1))::(truncateHead stack 2))     
-  in aux (List.rev l) []  
+      with FoundOpName opName ->
+        try aux tail (Op(opName, (List.nth stack 0), (List.nth stack 1))::(truncateHead stack 2)) with
+        Failure _ -> raise (WrongResult "Expression has less than expected tokens")
+  in aux (removeEl (List.rev l) ("")) []  
 
+(* analyse syntaxe to compose condition *)
 let analize_cond (l : string list) : cond =
-  let l1 = [] in
   let rec aux l acc =
   match l with
   | [] -> raise (WrongResult (" wrong comparaison"))
@@ -123,7 +132,18 @@ let analize_cond (l : string list) : cond =
     | a -> aux tail (a::acc)
   in aux l []
 
-(********************** Auxiliary functions *****************************)
+(* analyse syntaxe to compose Set *)
+let analyse_set (l : string list) : instr=
+  let rec aux l acc =
+    match l with
+    | [] -> raise (WrongResult (" wrong Set"))
+    | h::tail -> 
+      match h with
+      |":=" -> Set (get_name(analize_expr (List.rev acc)), analize_expr tail)
+      | a -> aux tail (a::acc)
+  in aux l []
+
+(******************************************* Read functions ******************************************************)
 
 (* read code from file and store it to list *)
 let read_file (filename:string) : ((int * string) list) =  
@@ -136,102 +156,96 @@ let read_file (filename:string) : ((int * string) list) =
     | None   -> close_in chan;
     List.rev acc in aux 1 []
 
-(* count number of blancs (offset) in line (string), if it is odd - exception *)
-let count_offset st : int =                       (* may be dont need this function *)
-  let rec aux st i acc =
-    match st.[i] with
-    | ' ' -> aux st (i+1) (acc + 1)
-    | _   -> if (acc mod 2 = 0) then acc
-    else raise (ErrorCountOffsets acc)       (*TODO may be raise ?*)
-  in aux st 0 0
+(* count block offset, each offset = 2 spaces, if it is odd - exception *)
+  let rec block_offset (l : string list) (offset : int) : int =    
+    match l with
+    | [] -> empty_string
+    | h::tail -> 
+      match h with
+      | "" -> block_offset tail (offset + 1)
+      | "\n" ->  empty_string
+      | _ ->  
+        if (offset mod 2 = 0) then offset / 2 
+             else raise (ErrorCountOffsets offset) 
 
-let splite_line (line : int * string) : string list =
+(******************************************* Split functions***********************************************************)
+let split_line (line : int * string) : string list =
   let (x, y) = line in    
   let splited = String.split_on_char ' ' y in splited
 
-let split_all_code (code : (int * string) list) : (int * (string list)) list =
+let split_all_code (code : (int * string) list) : code_lines_t =
   let rec aux acc code =
     match code with
     | [] -> acc
     | h::tail -> let (x, y) = h in
-     aux ((x, splite_line h)::acc) tail
+     aux ((x, split_line h)::acc) tail
   in aux [] (List.rev code)
 
-let compose_program (l : (int * (string list)) list) : program =   
-(*let rec aux acc p list =       (* inspect lines of code p : position; acc : accumulator for program*)
-  match list with  
-  | [] -> acc
-  | h::tail -> let (x, y) = h in    (* inspect words of line x : int , y : string list *)
-  let rec aux2 i offset =
-    match y with
-    | [] -> aux acc (p+1) list      (* if line is numerated but empty.. what if it is the last one ?*)
-    | a::tail2 -> match a with
-      | "" -> aux2 i (offset+1)               (* it's an offset *)          
-      | "READ" -> let e = get_name(analize_expr tail2) in            (* need to check if is number after READ *)
-     let name = Read e in
-     let acc = (p,i)::acc in aux acc (p+1)   
-      | "PRINT" -> (*let e = analize_expr tail2 in            (* need to check if is number after READ *)
-        let e = Print e in
-        let acc = (p,i)::acc in aux acc (p+1) *)failwith "TODO"              (* need to check if is instr after PRINT *)
-      | "IF" -> failwith "TODO"                 (* need to check if is cond*instr*instr after IF and create the blocks *)
-      | "WHILE" -> failwith "TODO"
-      | "COMMENT" -> failwith "TODO"            (* need ? *)
-      | _ -> failwith "TODO"       ":="  (*filling of TabName*)        (* it's a variable *)
-    in aux2 0 0
-  in aux [] 1 l*)failwith "TODO"
+(******************************************* Build block***************************************************************)
 
+(* append element to list and return obtained list*)
+let rec append l i =
+  match l with
+  | [] -> [i]
+  | h::t -> h::(append t i)
 
-(* TMP *)
-let get_string (l : (int * string) list) i : string =  
-  let el = List.nth l i in
-  let (x,y) = el in y
+(* check the syntax of potential ELSE block, 
+return true if it is correct and the block ELSE present, and false if it is not present (IF without ELSE) *)
+let rec is_else (_p, line) (exp_offset: int) (offset:int) (p : int) : bool =
+  match line with
+  | [] -> false
+  | x::xs -> 
+    match x with
+      | "" -> is_else (_p, xs) (exp_offset) (offset+1) (p)
+      | "ELSE" -> if ((exp_offset*2)=offset) then true 
+                  else let err_msg = Printf.sprintf "Wrong ELSE alignment at line  %d" p in 
+                  raise (WrongResult err_msg)
+      | _ -> false      
 
-(***************************  Prints  ***********************************)
-(* print simple list *)
-let print_list l =
-  let rec print_elements = function
-    | [] -> ()
-    | h::tail -> print_string h;
-    print_string ";";
-    print_elements tail
-  in
-  print_string "[";
-  print_elements l;
-  print_string "]"
-  (*print_string "\n"*)
-
-let print_tuple_int_st (t : (int * string list)) : unit =
-  let (x,y) = t in
-  print_string "(";
-  print_int x;
-  print_string ",";
-  print_list y;
-  print_string ")";
-  print_string "\n"
-
-let print_list_of_tuples (l : (int * (string list)) list) : unit =   
-  print_string "[";
-  List.iter (fun (h) -> let (x,y) = h in print_tuple_int_st h) l;
-  print_string "]";
-  print_string "\n"
-  
-
-(* TMP *)
-let print_list_tuples_1 (t : (int * string) list) : unit =
-  let rec print_elements = function
-    | [] -> ()
-    | h::tail -> let (x,y) = h in
-    print_string "(";
-    print_int x; print_string ","; print_string y;
-    print_string ");";
-    print_string "\n";
-    print_elements tail
-  in
-  print_string "[";
-  print_elements t;
-  print_string "]";
-  print_string "\n"
-
+(* compose block of programe *)
+let build_block (txt : code_lines_t) : block =   
+  let rec scan_lines (lines : code_lines_t) (cur_block : block) (offset : int) : (code_lines_t * block) =  
+    match lines with  
+    | [] -> ([], cur_block)
+    | h::tail -> 
+      let (p, line) = h in 
+      let cur_offset = block_offset line 0 in
+      if (cur_offset = offset) then (* line belongs to current block*)
+        let rec scan_instruction (l : string list) (c_code : code_lines_t) (c_bl: block) (c_off : int) (p:int) : (code_lines_t * block) =
+          match l with
+          | [] -> (tail, c_bl)  (* end of line *) 
+          | x::xs -> 
+            match x with
+            | ""        -> scan_instruction (xs) (c_code) (c_bl) (c_off) (p)
+            | "COMMENT" -> scan_lines (tail) (cur_block) (offset)
+            | "WHILE"   -> let (rest, sub_block) = scan_lines (tail) [] (c_off + 1) in
+                            let wh = (p, (While (analize_cond xs, sub_block))) in
+                            scan_lines (rest) (append c_bl wh) (c_off) (*continue scan the rest*)
+            | "IF"      -> let (rest, sub_bl1) = scan_lines (tail) [] (c_off + 1) in
+                              let else_line = try (List.hd rest) with Failure _ -> (0, []) in
+                              if is_else (else_line) (c_off) (0) (p) then
+                                let (else_tail, sub_bl2) = scan_lines (List.tl rest) [] (c_off + 1) in
+                                let if_else = (p, (If ((analize_cond xs), sub_bl1, sub_bl2))) in
+                                  scan_lines (else_tail) (append c_bl if_else) (c_off) (*continue scan the rest*)
+                              else   
+                                let iff = (If ((analize_cond xs), sub_bl1, [])) in 
+                                scan_lines (rest) (append c_bl (p, iff)) (c_off) (*continue scan the rest*)
+            | "PRINT"   -> (tail, append c_bl (p, (Print (analize_expr xs))))  
+            | "READ"    -> (tail, append c_bl (p, (Read (get_name(analize_expr xs)))))  
+            | _         -> (tail, append c_bl (p, (analyse_set line)))
+        in let (rest, cur_block) = scan_instruction (line) (lines) (cur_block) (offset) (p) 
+        in scan_lines (rest) (cur_block) (offset)
+      else if (cur_offset < offset) then (* line belongs to one of the parent blocks*)
+        (lines, cur_block) 
+      else if (cur_offset = empty_string) then (* if empty_line ...*)
+        scan_lines (tail) (cur_block) (offset)      
+      else  (*exception - wrong alignment*)
+        let err_msg = Printf.sprintf "Wrong block alignment at line   %d" p in 
+        raise (WrongResult err_msg)
+  in match scan_lines txt [] 0 with
+    | (_, root_block) -> root_block
+ 
+(******************************************* Print functions **********************************************************)
 let print_op (o: op) : unit =
   match o with
   | Add -> print_string "+"  
@@ -253,53 +267,60 @@ let rec print_expr (e: expr) : unit =
   match e with
   | Num x -> print_int x
   | Var v -> print_string v
-  | Op (op, e1, e2) -> print_string "("; print_op op; print_string ","; print_expr e1; print_string ","; print_expr e2; print_string ")"
+  | Op (op, e1, e2) -> print_op op; print_string " "; print_expr e1; print_string " "; print_expr e2
 
+let print_cond (c: cond) : unit =    
+  let (ex1, compar, _ex2) = c in
+  print_expr ex1; print_string " "; print_comp compar; print_string " "; print_expr _ex2
+ 
+let rec print_align (depth:int) : unit =
+  match depth with
+  | 0 -> ()
+  | _ -> print_string "  "; print_align (depth - 1)
+    
+let rec print_block (b: block) (d : int) : unit =    
+    match b with
+    | [] -> () 
+    | h::tail -> let (_, l) = h in   
+    match l with
+    | Set (n, ex)      -> print_align d; 
+                          print_string n; print_string " := "; print_expr ex; print_string "\n"; 
+                          print_block tail d
+    | Read n           -> print_align d; 
+                          print_string "READ "; print_string n; print_string "\n"; 
+                          print_block tail d
+    | Print ex         -> print_align d; 
+                          print_string "PRINT "; print_expr ex; print_string "\n"; 
+                          print_block tail d
+    | If (c, bl1, bl2) -> print_align d; 
+                          print_string "IF "; print_cond c; print_string "\n";
+                          print_block bl1 (d + 1);       
+                          if (List.length bl2) > 0 then print_align d; 
+                          if (List.length bl2) > 0 then print_string "ELSE\n";
+                          print_block bl2 (d + 1); print_block tail d
+    | While (c, bl)    -> print_align d; 
+                          print_string "WHILE "; print_cond c; print_string "\n";
+                          print_block bl (d + 1); 
+                          print_block tail d
 
-let print_cond (c: cond) : unit =
-  let (e1, comp, e2) = c in
-  print_string "(";
-  print_expr e1;
-  print_comp comp;
-  print_expr e2;  
-  print_string ")"
-  (* print_string "("; print_op op; print_string ","; print_expr e1; print_string ","; print_expr e2; print_string ")"*)
-(************************  Evaluation  ******************************)
-
-(************************  Main functions  ******************************)
-
+(******************************************* Main functions ***********************************************************)
 let read_polish (filename:string) : program = 
-  compose_program(split_all_code(read_file(filename)))
+  build_block(split_all_code(read_file(filename)))
 
-let print_polish (p:program) : unit = failwith "TODO"
+let print_polish (p:program) : unit = 
+  print_block (p) 0
 
 let eval_polish (p:program) : unit = failwith "TODO"
 
 let usage () =
-  (*print_list_tuples (read_file "/home/nata/Documents/L3_PF/pf5-projet/exemples/fact.p");  
-  print_list_tuples (read_file "/home/nata/Documents/L3_PF/pf5-projet/exemples/factors.p");  
-  print_string "\n"; *)
-  print_expr(analize_expr (["+";"1";"*";"3";"n";]));
+  print_string "Polish : analyse statique d'un mini-langage\n";
+  print_string "usage: à documenter (TODO)\n"; 
+  print_string "\n"; 
+  print_polish (read_polish "./exemples/factors.p");
   print_string "\n";
-  print_expr(analize_expr (["1"]));
+  print_polish (read_polish "./exemples/fibo.p");
   print_string "\n";
-  print_expr(analize_expr (["name"]));
-  print_string "\n";
-  print_expr(analize_expr (["+";"-";"20";"*";"3";"4";"1";]));
-  print_string "\n";
-  print_cond(analize_cond(["n";"<";"-";"4";"1";]));
-  print_string "\n";
-  print_cond(analize_cond(["-";"n";"20";"<";"-";"4";"1";]));
-  print_string "\n";
-
-  print_expr(analize_expr (["+";"-";"20";"*";"3";"4";]));           (* wrong expression*)
-  print_string "\n";
-  print_expr(analize_expr (["+";"-";"20";"*";"3";"4";"-";]));           (* wrong expression*)
-  print_string "\n";
-  print_expr(analize_expr (["%"]));
-  print_string "\n"
-  (*print_string "Polish : analyse statique d'un mini-langage\n";
-  print_string "usage: à documenter (TODO)\n"*)
+  print_polish (read_polish "./exemples/syracuse.p")
 
 let main () =
   match Sys.argv with
