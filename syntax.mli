15cf2f7cd5d4f836a738333585cddb0cc318adcc
GOLIKOVA anna <anna.golikova@gmail.com>
2022-01-06 22:02:49 +0100

    Update syntax.mli

--- syntax.mli
+++ syntax.mli
@@ -3,5 +3,8 @@ open Types
 val build_block : code_lines_t -> block
 (** builds a program block from a choosen segment *)
 
+val append : 'a list -> 'a -> 'a list
+(** appends an element *)
+
 val get_name : expr -> name 
 (** gets name from expression *)
