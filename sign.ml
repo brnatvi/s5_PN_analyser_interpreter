16ae6938488a3ee5a349e4b1eface0c496206599
BRAGINA natalia <nata.br@gmail.com>
2022-01-09 12:02:51 +0100

    sign of expression

--- sign.ml
+++ sign.ml
@@ -6,11 +6,11 @@ open Vars
 open Simpl
 
 let sign_compare(s1: sign)(s2: sign) : int =
-  if s1 == s2 then 0
+  if s1 = s2 then 0
   else if s1 > s2 then 1
-  else -1 
+  else -1
 
-(*prints*)
+(* prints *)
 let rec print_signes (s : sign list) : unit =
   match s with
   | [] -> Printf.printf "\n"
@@ -25,30 +25,20 @@ let rec print_signes (s : sign list) : unit =
 let print_vars_signes (m : sign list NameTable.t) : unit =
   NameTable.iter (fun key value -> Printf.printf "%s " key; print_signes (List.sort sign_compare value)) m
 
-(* comparation *)
+(* equality tests *)
 let is_signes_equal(s1 : sign list)(s2 : sign list) : bool =
   let rec aux l1 l2 =
     match l1, l2 with
     | [], [] -> true
     | [], _ -> false
     | _, [] -> false
-    | c::cc, d::dd -> if c == d then aux cc dd else false
+    | c::cc, d::dd -> if c = d then aux cc dd else false
   in aux (List.sort sign_compare s1) (List.sort sign_compare s2)
 
 let is_vars_signes_equal(m1 : sign list NameTable.t) (m2 : sign list NameTable.t): bool =
   NameTable.equal is_signes_equal m1 m2
 
-(* add *)  
-let rec add_signes(sl1 : sign list) (sl2 : sign list): sign list =
-  let add_one_sign s sl = 
-    try (let _ = List.find (fun a -> a = s) sl in sl ) with Not_found -> s::sl in
-      match sl1 with
-      | [] -> sl2
-      | h::t -> let sl2 = add_one_sign h sl2 in add_signes t sl2
-
-
-
-(* analysis of cases*)  
+(* analize of cases*)  
 let get_plus_signes(s1:sign)(s2:sign): sign list =
   match s1, s2 with
   | Pos, Pos
@@ -105,7 +95,7 @@ let get_div_signes(s1:sign)(s2:sign): sign list =
   | Pos, Neg   -> [Neg]
   | Neg, Neg   -> [Pos] 
 
-let get_bop_signes(o:op)(s1:sign)(s2:sign): sign list =
+let get_bop_signes(o:op)(s1:sign)(s2:sign) : sign list =
   match o with
   | Add -> get_plus_signes s1 s2
   | Sub -> get_minus_signes s1 s2
@@ -113,14 +103,40 @@ let get_bop_signes(o:op)(s1:sign)(s2:sign): sign list =
   | Div
   | Mod -> get_div_signes s1 s2
 
-  
-let get_block_signes (bl : block) (m : sign list NameTable.t) (ln : int) : (int * sign list NameTable.t) =
+(* sign of expression *)
+let rec merge_lists_signes(sl1 : sign list) (sl2 : sign list): sign list =
+  let add_one_sign s sl = 
+    if List.mem s sl then sl else s::sl in    
+      match sl1 with
+      | [] -> sl2
+      | h::t -> let sl2 = add_one_sign h sl2 in merge_lists_signes t sl2  
+
+let rec get_signes(o : op)(s : sign)(sl : sign list)(res : sign list) : sign list =
+  match sl with
+  | []      -> res
+  | h::tail -> let res = merge_lists_signes (get_bop_signes o s h) res in get_signes o s tail res
+
+let rec deduct_signes_op (o : op)(sl1 : sign list)(sl2: sign list)(res : sign list) : sign list =
+    match sl1 with
+    | []      -> res
+    | h::tail -> let res = merge_lists_signes (get_signes o h sl2 []) res in deduct_signes_op o tail sl2 res
+    
+let rec get_expression_signes(e : expr)(m : sign list NameTable.t) : sign list = 
+  match e with
+  | Num x -> if (x = 0) then [Zero] else if x > 0 then [Pos] else [Neg]
+  | Var v -> (try (NameTable.find v m) with Not_found -> [])           
+  | Op (o, x1, x2) -> deduct_signes_op o (get_expression_signes x1 m) (get_expression_signes x2 m) []
+
+
+
+(* principal functions *)
+
+let get_block_signes (bl : block) (m : sign list NameTable.t) (pErr : int) : (int * sign list NameTable.t) =
   failwith "TODO"
   
 let sign_polish (p:program) : unit =
-  let (ln, p) = get_block_signes p (NameTable.empty) (-1) in
-  let () = print_vars_signes p in
-    if (ln > -1) then Printf.printf "divbyzero %d\n" ln
+  let (pErr, p) = get_block_signes p (NameTable.empty) (-1) in
+  let () = print_vars_signes (p) in
+    if (pErr > -1) then Printf.printf "divbyzero %d\n" pErr
     else Printf.printf "safe\n"
-
 
